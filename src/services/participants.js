3f3c0134a095fb04ae72d1b95132ee60be2afca2
Lorenzo <lorenzo.serminato@gmail.com>
2025-11-29 01:07:03 +0100

    ok

--- src/services/participants.js
+++ src/services/participants.js
@@ -1,4 +1,4 @@
-import { supabase } from '../utils/supabaseClient';
+import { supabase } from '@/lib/supabaseClient';
 
 export const fetchParticipants = async () => {
   const { data, error } = await supabase
